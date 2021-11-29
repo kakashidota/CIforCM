0e8a29beaa840ff07472fb333fe901ed4dabef97
Kakashidota <robin.kamo@live.se>
2021-11-29 20:36:50 +0100

    Adding files

---
+++ test_app.py
@@ -0,0 +1,17 @@
+import pytest
+from app import app
+
+
+@pytest.fixture
+def client():
+    return app.test_client()
+
+
+def test_home_page(client):
+    response = client.get("/")
+    assert response.status_code == 200
+
+
+def test_contact_page(client):
+    reponse = client.get("/contract")
+    assert reponse.status_code == 200
